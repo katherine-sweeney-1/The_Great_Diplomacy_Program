cf77db40f4bc7aa3aa98723db9edd62616313036
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2025-12-20 11:06:17 -0500

    need to debug support issue

--- data/Txt_Hard_Data/Cmdrs_8.py
+++ data/Txt_Hard_Data/Cmdrs_8.py
@@ -271,8 +271,26 @@ cmdrs_8_1909 = {
                    },
     "Michael" : {"Country": "TU", 
                    "Unit Members": ["TU01", "TU02", "TU03", "TU04", "TU05", "TU06", "TU07", "TU08", "TU09", "TU10", "TU11", "TU12", "TU13"],
-                   "Dots Owned": ["Smy", "Ank", "Con", "Bul", "Gre", "Ser", "Rum", "Tri", "Vie", "Bud", "Ven", "Nap", "Rom", "Sev", "Mos", "War"]
+                   "Dots Owned": ["Smy", "Ank", "Con", "Bul", "Gre", "Ser", "Rum", "Tri", "Vie", "Bud", "Ven", "Nap", "Rom", "Sev", "War"]
                    }
 }
 
+cmdrs_8_1910 = {
+    "Mercy" : {"Country": "UK", 
+                   "Unit Members": ["UK01", "UK02", "UK03", "UK04", "UK05", "UK06", "UK07", "UK08", "UK09"],
+                   "Dots Owned": ["Edi", "Lon", "Lvp", "Nwy", "Swe", "Den", "Stp", "Mos", "Hol"]
+                   },
+    "Katherine" : {"Country": "FR",
+                    "Unit Members": ["FR01", "FR02", "FR03", "FR04", "FR05", "FR06", "FR07", "FR08", "FR09", "FR10"],
+                    "Dots Owned": ["Par", "Mar", "Bre", "Spa", "Por", "Bel", "Tun", "Mun", "Kie", "War"]
+                    },
+    "Will" : {"Country": "RU", 
+                   "Unit Members": ["RU01"],
+                   "Dots Owned": ["Ber"]
+                   },
+    "Michael" : {"Country": "TU", 
+                   "Unit Members": ["TU01", "TU02", "TU03", "TU04", "TU05", "TU06", "TU07", "TU08", "TU09", "TU10", "TU11", "TU12", "TU13", "TU14"],
+                   "Dots Owned": ["Smy", "Ank", "Con", "Bul", "Gre", "Ser", "Rum", "Tri", "Vie", "Bud", "Ven", "Nap", "Rom", "Sev"]
+                   }
+}
 
