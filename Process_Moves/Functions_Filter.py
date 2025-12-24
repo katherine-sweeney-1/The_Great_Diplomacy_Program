11b3a352d7ab2fb78f1d33e26905951563065e33
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2025-12-24 14:51:21 -0500

    debugging convoy filters

--- Process_Moves/Functions_Filter.py
+++ Process_Moves/Functions_Filter.py
@@ -167,7 +167,7 @@ def filter_commands(commands, commanders):
         command = filtered_commands[command_id]
         command = filter_support(command, filtered_commands)
         # commenting out for convoys
-        
+        """
         if command.legal != 1:
             invalid_commands[command_id] =command
             command.origin = command.location
@@ -175,6 +175,6 @@ def filter_commands(commands, commanders):
             valid_commands[command_id] = command
         else:
             valid_commands[command_id] = command
-        
+        """
         valid_commands[command_id] = command
     return valid_commands, invalid_commands
