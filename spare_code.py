71b522d65778f28ebd1f4be465308f772d137d9d
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2026-01-25 09:51:38 -0500

    restructured code

--- spare_code.py
+++ spare_code.py
@@ -1,11 +1,13 @@
-import math
-
-# Check whether some values are NaN or not
-print (math.isnan (56), type(56))
-print (math.isnan (-45.34), type(-45.34))
-print (math.isnan (+45.34))
-print (math.isnan (math.inf))
-print (math.isnan (float("nan")), type(float("nan")))
-print (math.isnan (float("inf")))
-print (math.isnan (float("-inf")))
-print (math.isnan (math.nan))
+# Draw a line on map
+def draw_line_from_coordinates(first_coordinate, second_coordinate, command, drawing_image):
+    coordinates = [first_coordinate, second_coordinate]
+    origin_coordinate = first_coordinate
+    destination_coordinate = second_coordinate
+    if command.succeed == True:
+        fill = "black"
+    else:
+        fill = "red"
+    upper_coordinates, lower_coordinates = get_arrow_coordinates(origin_coordinate, destination_coordinate)
+    drawing_image.line(coordinates, fill, width = 2)
+    drawing_image.line(upper_coordinates, fill, width = 2)
+    drawing_image.line(lower_coordinates, fill, width = 2)
