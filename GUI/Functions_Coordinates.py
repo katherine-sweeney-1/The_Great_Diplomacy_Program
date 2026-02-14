987c6d4e8b186ddb1d77cc97c637905e753452ee
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2026-02-14 10:50:22 -0500

    need to debug last line

--- GUI/Functions_Coordinates.py
+++ GUI/Functions_Coordinates.py
@@ -47,21 +47,37 @@ def write_coordinates_file(territory_file, coordinates_file):
 def get_territories_with_neighbors_coordinates(nodes_data_main, territory_coordinates, territory_neighbor_coordinates):
     # each entry in the nodes dictionary
     territories_neighbors_with_coordinates = {}
+    coordinates_file = open(territory_coordinates)
+    """
+    for line in coordinates_file:
+        print(line)
+    """
     for node_entry in nodes_data_main:
         neighbors = nodes_data_main[node_entry]["Neighbors"]
         neighbors = neighbors.split(" ")
         # get the coordinates for each neighbor for each entry in the nodes dictionary
         neighbors_coordinates = {}
         for neighbor in neighbors:
+            line_count = 1
             with open(territory_coordinates, "r") as file_input, open(territory_neighbor_coordinates, "a") as file_output:
+                number_of_lines = 0
                 for line in file_input:
+                    number_of_lines += 1
+                for line in file_input:
+                    print("yes", line)
                     if line[0:3] == neighbor:
-                        coordinate_tuple = line[4:-1]
+                        if line_count != number_of_lines:
+                            coordinate_tuple = line[4:-1]
+                        else:
+                            coordinate_tuple = line[4:0]
                         neighbors_coordinates[line[0:3]] = coordinate_tuple
+                    line_count += 1
+            
         territories_neighbors_with_coordinates[node_entry] = neighbors_coordinates
         with open(territory_neighbor_coordinates, "a") as file_output:
             print(node_entry, territories_neighbors_with_coordinates[node_entry], file = file_output)
     return territories_neighbors_with_coordinates
+    
 
 def assign_coordinates_to_nodes(nodes, coordinate_file, coastal_coordinate_file):
     for node_id in nodes:
--- Run_TGDP/Main.py
+++ Run_TGDP/Main.py
@@ -2,8 +2,9 @@ from Run_Main import run_main_process_moves
 from Input_Commands_and_Commanders import *
 import sys
 sys.path.append("../The_Great_Diplomacy_Program/GUI")
-from Functions_GUI import run_gui, set_up_nodes
+from Functions_GUI import run_gui, retrieve_node_coordinates, assign_coordinates_to_nodes
 
 game_objects = run_main_process_moves(input_data_8b, "8")
-set_up_nodes()
+# retrieve_node_coordinates()
+assign_coordinates_to_nodes()
 #gui = run_gui(game_objects)
