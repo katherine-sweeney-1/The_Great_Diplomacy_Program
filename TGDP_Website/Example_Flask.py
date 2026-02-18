01885df1a456019d932e0660c804bfb243f006cf
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2026-02-18 09:47:17 -0500

    added test data; got flask to run on localhost

--- TGDP_Website/Example_Flask.py
+++ TGDP_Website/Example_Flask.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
 
+app = Flask(__name__, instance_relative_config = True)
+"""
+@app.route("/")
 def create_app(test_config = None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config = True)
@@ -19,6 +22,18 @@ def create_app(test_config = None):
     os.makedirs(app.instance_path, exist_ok = True)
     @app.route("/hello")
     def hello():
-        return "Hello World!"
+        return "Hello World!!!"
     return app
+"""
+app = Flask(__name__)
+
+@app.route("/https://the-great-diplomacy-program.netlify.app/")
+def hello():
+    return "Hello World!!!"
+
+# runs if I use http://127.0.0.1:5000/hello
+if __name__ == "__main__":
+    app.run("localhost", 5000)
+
+
 
