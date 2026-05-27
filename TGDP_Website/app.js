ccd20fcfb0f1d0d4a7217f2ffd310ada4c8bd49d
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2026-05-27 18:25:14 -0400

    Debuggin javascript, port issue

--- TGDP_Website/app.js
+++ TGDP_Website/app.js
@@ -1,10 +1,10 @@
 const express = require("express")
-const request = require("request");
+//const request = require("request");
 
 app = express();
 const PORT = 3000;
-
-app.get("/home", function(req, res){
+/*
+app.get("/", function(req, res){
     request("http://127.0.0.1:5500/example_save_output", function(error, response, body){
         console.error("error:", error); // print error
         console.log("statusCode:", response && response.statusCode); //print response
@@ -12,10 +12,72 @@ app.get("/home", function(req, res){
         res.send(body); //display response on website
     });
 });
+*/
 app.listen(PORT, function(){
-    console.log("listening on port 3000");
+    console.log("listening on port 5501");
+});
+/*
+app.get("/", (req, res) => {
+    res.send("GET request to the homepage")
+});
+
+app.post("/", (req, res) => {
+    res.send("POST request to the homepage")
+});
+*/
+app.get("/about", (req, res) => {
+    res.send("About page")
+});
+/*
+app.all('*', (req, res) =>{
+    res.status(404).send("404 - Page not found response")
+});
+*/
+
+app.listen(PORT, () => {
+    console.log("example listening at localhost:${PORT}")
 })
 
+app.post("/TGDP_Home.html", (req, res) => {
+    const data_dict = {}
+    console.log("checking example save output")
+    const data_button = document.getElementById("data-input-2").value
+    data_button.addEventListener("click", async_ => {
+        try {
+            Promise.all(
+                URLSearchParams.map(async (data_button) => {
+                    const response = await fetch(data_button);
+                }),
+            );
+            console.log("checking")
+            //const response = await fetch ("/TGDP_Home.html")
+            if (! response.ok){
+                throw new Error("Response status: ${response}");
+                }
+            const result = response.json();
+            console.log.result();
+        }
+        catch (error) {
+        console.error(error.message);
+        console.log("compeleted", response);
+        }
+    })
+})
+    
+/*  
+    console.log("received data -save output: ${data_button")
+    data_dict["data from site"] = data_button
+    var file = require("file"),  str  = data_dict;
+    // e = event
+    file.open("/Example_Submission.json", "a", 666, function(e, id) {
+        file.write(id, "string to append to file", null, "utf8", function(){
+            file.close(id, function(){
+                console.log("file closed");
+            });
+        });
+    });
+});
+*/
 /*
 app.route("/")
 def home():
@@ -23,7 +85,7 @@ def home():
     europe_map_url = "Static/Europe_Map.png"
     #europe_map_url = "/home/katherine/Documents/The_Great_Diplomacy_Program/TGDP_Website/Static/Europe_Map_For_Website.png"
     return render_template("TGDP_Home.html", image_url = europe_map_url)
-/*
+
 @app.route("/example", methods = ["GET", "POST"])
 def users():
     print("checking example")
